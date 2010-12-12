1028f9560b3227ffee8942e79f78b6fec7796d72
Chris Soyars <ctso@ctso.me>
2010-12-12 05:16:45 -0500

    Use package names from constants class.

--- src/com/cyanogenmod/gapps/DownloadActivity.java
+++ src/com/cyanogenmod/gapps/DownloadActivity.java
@@ -8,12 +8,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import com.cyanogenmod.gapps.utils.Constants;
 
 public class DownloadActivity extends Activity {
 
-    private String[] mApps;
     private CheckBox mGmail;
     private CheckBox mChromeToPhone;
     private CheckBox mGoogleGoggles;
@@ -34,9 +33,6 @@ public class DownloadActivity extends Activity {
         setContentView(R.layout.main);
         setTitle(R.string.activity_title);
 
-        //		Set the length of the array to the number of checkboxes initialized;
-        mApps = new String[9];
-
         mGmail = (CheckBox) findViewById(R.id.gmail);
         mChromeToPhone = (CheckBox) findViewById(R.id.chrometophone);
         mGoogleGoggles = (CheckBox) findViewById(R.id.googlegoggles);
@@ -50,107 +46,58 @@ public class DownloadActivity extends Activity {
         mOkButton = (Button) findViewById(R.id.main_btn_ok);
         mCancelButton = (Button) findViewById(R.id.main_btn_cancel);
 
-        mGmail.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[0] = "market://details?id=com.google.android.gm";
-                } else {
-                    mApps[0] = null;
-                }
-            }
-        });
-        mChromeToPhone.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[1] = "market://details?id=com.google.android.apps.chrometophone";
-                } else {
-                    mApps[1] = null;
-                }
-            }
-        });
-        mGoogleGoggles.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[2] = "market://details?id=com.google.android.apps.unveil";
-                } else {
-                    mApps[2] = null;
-                }
-            }
-        });
-        mGoogleMaps.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[3] = "market://details?id=com.google.android.apps.maps";
-                } else {
-                    mApps[3] = null;
-                }
-            }
-        });
-        mGoogleSearch.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[4] = "market://details?id=com.google.android.googlequicksearchbox";
-                } else {
-                    mApps[4] = null;
-                }
-            }
-        });
-        mGoogleVoice.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[5] = "market://details?id=com.google.android.apps.googlevoice";
-                } else {
-                    mApps[5] = null;
-                }
-            }
-        });
-        mStreetView.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[6] = "market://details?id=com.google.android.street";
-                } else {
-                    mApps[6] = null;
-                }
-            }
-        });
-        mVoiceSearch.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[7] = "market://details?id=com.google.android.voicesearch";
-                } else {
-                    mApps[7] = null;
-                }
-            }
-        });
-        mYoutube.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked){
-                    mApps[8] = "market://details?id=com.google.android.youtube";
-                } else {
-                    mApps[8] = null;
-                }
-            }
-        });
-
         mOkButton = (Button) findViewById(R.id.main_btn_ok);
         mOkButton.setOnClickListener(new OnClickListener(){
-            public void onClick(View arg0){
-                for(int k = 0; k < mApps.length; k++){
-                    if(mApps[k] != null) {
-                        Uri uri = Uri.parse(mApps[k]);
-                        startActivity(new Intent(Intent.ACTION_VIEW, uri));
-                    }
-                };
+            public void onClick(View view){
+                if (mGmail.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_GMAIL));
+                }
+
+                if (mChromeToPhone.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_CHROME));
+                }
+
+                if (mGoogleGoggles.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_GOGGLES));
+                }
+
+                if (mGoogleMaps.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_MAPS));
+                }
+
+                if (mGoogleSearch.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_GOOGLESEARCH));
+                }
+
+                if (mGoogleVoice.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_VOICE));
+                }
+
+                if (mStreetView.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_STREETVIEW));
+                }
+
+                if (mVoiceSearch.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_VOICESEARCH));
+                }
+
+                if (mYoutube.isChecked()) {
+                    startActivity(getIntent(Constants.PNAME_YOUTUBE));
+                }
             }
         });
         mCancelButton = (Button) findViewById(R.id.main_btn_cancel);
         mCancelButton.setOnClickListener(new OnClickListener(){
-            public void onClick(View arg0){
-                for(int k = 0; k < mApps.length; k++){
-                    mApps[k] = null;
-                }
+            public void onClick(View view){
                 finish();
             }
         });
+    }
+
+    private Intent getIntent(String pname) {
+        Uri uri = Uri.parse("market://details?id=" + pname);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+        return intent;
     }
 }
---
+++ src/com/cyanogenmod/gapps/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.cyanogenmod.gapps.utils;
+
+public class Constants {
+    public static final String PNAME_GMAIL = "com.google.android.gm";
+    public static final String PNAME_CHROME = "com.google.android.apps.chrometophone";
+    public static final String PNAME_GOGGLES = "com.google.android.apps.unveil";
+    public static final String PNAME_MAPS = "com.google.android.apps.maps";
+    public static final String PNAME_GOOGLESEARCH = "com.google.android.googlequicksearchbox";
+    public static final String PNAME_VOICE = "com.google.android.apps.googlevoice";
+    public static final String PNAME_STREETVIEW = "com.google.android.street";
+    public static final String PNAME_VOICESEARCH = "com.google.android.voicesearch";
+    public static final String PNAME_YOUTUBE = "com.google.android.youtube";
+}
