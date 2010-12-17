885a45cf2fd5e875e5c8430283cb60f79161337f
Keyan Mobli <persiansown@gmail.com>
2010-12-17 17:19:40 -0600

    Change to pacman

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := CMGapps
+LOCAL_PACKAGE_NAME := Pacman 
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
