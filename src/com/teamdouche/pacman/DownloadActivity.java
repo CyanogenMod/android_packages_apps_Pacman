e12c7cbfc379a23e7051bd500201887ad8e205c9
Keyan Mobli <persiansown@gmail.com>
2011-01-17 15:09:04 -0600

    Allow user to defer pacman to later date
    Change-Id: I0009186ef38181cdbcb471aa7daa7b58065045f4

--- src/com/teamdouche/pacman/DownloadActivity.java
+++ src/com/teamdouche/pacman/DownloadActivity.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
