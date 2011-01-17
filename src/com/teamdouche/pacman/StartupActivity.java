440f83c2bad4735533d0e1ce94e88db1facd3cfe
Keyan Mobli <persiansown@gmail.com>
2011-01-17 15:09:04 -0600

    Allow user to defer pacman to later date
    Change-Id: I0009186ef38181cdbcb471aa7daa7b58065045f4

--- src/com/teamdouche/pacman/StartupActivity.java
+++ src/com/teamdouche/pacman/StartupActivity.java
@@ -1,22 +1,39 @@
 package com.teamdouche.pacman;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
 public class StartupActivity extends Activity {
+    private AlertDialog alertDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.v("Pacman", "Starting Pacman Download Activity");
-        Intent starterIntent = new Intent(this, DownloadActivity.class);
-        startActivityForResult(starterIntent, 0);
+        // Set up the warning
+        alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(R.string.alert_dialog_title);
+        alertDialog.setMessage(getResources().getString(R.string.alert_dialog_message));
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Log.v("Pacman", "Starting Pacman Download Activity");
+                Intent starterIntent = new Intent(getApplicationContext(), DownloadActivity.class);
+                startActivityForResult(starterIntent, 0);
+                return;
+            }
+        });
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        alertDialog.show();
     }
 
     @Override
